ff2059810c805b6e2bcc3777001e49e0f56d9fdf
Philip Bordallo <git@philipbordallo.com>
2019-09-08 14:22:45 -0400

    Add postcss build process

---
+++ postcss.config.js
@@ -0,0 +1,33 @@
+const autoprefixer = require('autoprefixer');
+const atImport = require('postcss-import');
+const cssnano = require('cssnano');
+const systemMonospace = require('postcss-system-monospace');
+const systemUI = require('postcss-font-family-system-ui');
+const colorEmoji = require('postcss-color-emoji');
+const pseudoClassAnyLink = require('postcss-pseudo-class-any-link');
+const nesting = require('postcss-nesting');
+const inputRange = require('postcss-input-range');
+
+
+module.exports = (context) => {
+  const isProduction = context.env === 'production';
+
+  return {
+    map: false,
+    plugins: [
+      atImport,
+      nesting,
+      systemMonospace,
+      systemUI({
+        family: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Noto Sans, Ubuntu, Cantarell, Helvetica Neue'
+      }),
+      colorEmoji,
+      pseudoClassAnyLink({
+        preserve: false
+      }),
+      inputRange,
+      autoprefixer,
+      isProduction ? cssnano : null
+    ].filter(Boolean),
+  }
+};
